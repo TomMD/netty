ec3e4dee14c170689c94eb9c7153664765ac89dd
xingrufei <qhdxssm@qq.com>
2020-07-23 13:31:02 +0800

    add license to file

--- codec/src/main/java/io/netty/handler/codec/compression/ZstdConstants.java
+++ codec/src/main/java/io/netty/handler/codec/compression/ZstdConstants.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package io.netty.handler.codec.compression;
 
 final class ZstdConstants {
@@ -15,7 +30,13 @@ final class ZstdConstants {
      */
     static final int DEFAULT_COMPRESS_LEVEL = 0;
     static final int TOKEN_OFFSET = 4;
+    /**
+     * Compressed length offset
+     */
     static final int COMPRESSED_LENGTH_OFFSET = TOKEN_OFFSET + 1;
+    /**
+     * Decompressed length offset
+     */
     static final int DECOMPRESSED_LENGTH_OFFSET = COMPRESSED_LENGTH_OFFSET + 4;
     static final int DEFAULT_CHECKSUM_OFFSET = DECOMPRESSED_LENGTH_OFFSET + 4;
     static final int COMPRESSION_LEVEL_BASE = 10;
@@ -27,6 +48,9 @@ final class ZstdConstants {
      * Max block size
      */
     static final int MAX_BLOCK_SIZE = 1 << COMPRESSION_LEVEL_BASE + 0x0F;   //  32 M
+    /**
+     * Default block size
+     */
     static final int DEFAULT_BLOCK_SIZE = 1 << 16;  // 64 KB
     static final int BLOCK_TYPE_NON_COMPRESSED = 0x10;
     static final int BLOCK_TYPE_COMPRESSED = 0x20;
